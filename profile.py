b0613d0c900c45e060dc14b4de2243cf6993d751
sahaj jain <37249534+ssaahhaajj@users.noreply.github.com>
2020-01-15 01:52:09 +0530

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,12 @@
+from thop import profile as pf_flop
+import torchprof as pf_time
+import pandas as DataFrame
 
+def profile(model, inp_data, want_op_file=False, cuda_=False):
+  df1 = pf_flop(model, inputs=(inp_data, ))  
+  data, targets = get_batch(val_data, 0)
+  with pf_time.Profile(model, use_cuda=cuda_) as prof:
+    model(inp_data)
+  df2=prof.display()
+  print(df1)
+  print(df2)
