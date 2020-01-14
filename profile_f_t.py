b7334de6b2984013b0eb1c042633ad7a32c63f7f
sahaj jain <37249534+ssaahhaajj@users.noreply.github.com>
2020-01-15 02:42:57 +0530

    Update profile_f_t.py

--- profile_f_t.py
+++ profile_f_t.py
@@ -9,15 +9,15 @@ def profile(model, inp_data, want_op_file=False, cuda_=False):
   df2=prof.display()
   mynn={"Layer Name":[],"FLOPs":[],"Self CPU total":[], "CPU Total":[], "GPU Total":[],"Input Features":[], "Output Features":[], "Dict Size of Emb":[], "Emb Vector Size":[], "Norm Size":[]}
   for i1 in df1.index:
-    mynn["Layer Name"].append(df2["Layer Name"][i1])
-    mynn["Self CPU total"].append(df2["Self CPU total"][i1])
-    mynn["CPU Total"].append(df2["CPU total"][i1])
-    mynn["GPU Total"].append(df2["GPU total"][i1])
-    mynn["Input Features"].append(df1["Input Features"][i1])
-    mynn["Output Features"].append(df1["Output Features"][i1])
-    mynn["Dict Size of Emb"].append(df1["Dict Size of Emb"][i1])
-    mynn["Emb Vector Size"].append(df1["Emb Vector Size"][i1])
-    mynn["Norm Size"].append(df1["Norm Size"][i1])
+    mynn["Layer Name"].append(str(df2["Layer Name"][i1]))
+    mynn["Self CPU total"].append(str(df2["Self CPU total"][i1]))
+    mynn["CPU Total"].append(str(df2["CPU total"][i1]))
+    mynn["GPU Total"].append(str(df2["GPU total"][i1]))
+    mynn["Input Features"].append(str(df1["Input Features"][i1]))
+    mynn["Output Features"].append(str(df1["Output Features"][i1]))
+    mynn["Dict Size of Emb"].append(str(df1["Dict Size of Emb"][i1]))
+    mynn["Emb Vector Size"].append(str(df1["Emb Vector Size"][i1]))
+    mynn["Norm Size"].append(str(df1["Norm Size"][i1]))
     
   df=DataFrame(mynn, columns= ["Layer Name","FLOPs","Self CPU total","CPU Total","GPU Total","Input Features","Output Features","Dict Size of Emb","Emb Vector Size","Norm Size"])
   if want_op_file==True:
