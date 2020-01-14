e7233fbdc0f65761272425045f40b422f1ab44f7
sahaj jain <37249534+ssaahhaajj@users.noreply.github.com>
2020-01-15 02:01:15 +0530

    Update profile_f_t.py

--- profile_f_t.py
+++ profile_f_t.py
@@ -4,7 +4,6 @@ import pandas as DataFrame
 
 def profile(model, inp_data, want_op_file=False, cuda_=False):
   df1 = pf_flop(model, inputs=(inp_data, ))  
-  data, targets = get_batch(val_data, 0)
   with pf_time.Profile(model, use_cuda=cuda_) as prof:
     model(inp_data)
   df2=prof.display()
