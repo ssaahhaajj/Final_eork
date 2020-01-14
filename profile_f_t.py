508278f246d991fece629c05f1a1863f34107020
sahaj jain <37249534+ssaahhaajj@users.noreply.github.com>
2020-01-15 02:33:54 +0530

    Update profile_f_t.py

--- profile_f_t.py
+++ profile_f_t.py
@@ -4,11 +4,9 @@ import pandas as DataFrame
 
 def profile(model, inp_data, want_op_file=False, cuda_=False):
   df1 = pf_flop(model, inputs=(inp_data, ))  
-  
   with pf_time.Profile(model, use_cuda=cuda_) as prof:
     model(inp_data)
   df2=prof.display()
-  
   mynn={"Layer Name":[],"FLOPs":[],"Self CPU total":[], "CPU Total":[], "GPU Total":[],"Input Features":[], "Output Features":[], "Dict Size of Emb":[], "Emb Vector Size":[], "Norm Size":[]}
   for i1 in df1.index:
     mynn["Layer Name"].append(df2["Layer Name"][i1])
@@ -20,9 +18,7 @@ def profile(model, inp_data, want_op_file=False, cuda_=False):
     mynn["Dict Size of Emb"].append(df1["Dict Size of Emb"][i1])
     mynn["Emb Vector Size"].append(df1["Emb Vector Size"][i1])
     mynn["Norm Size"].append(df1["Norm Size"][i1])
-  
-  df3 = DataFrame(mynn, columns= ["Layer Name","FLOPs","Self CPU total","CPU Total","GPU Total","Input Features","Output Features","Dict Size of Emb","Emb Vector Size","Norm Size"])
- 
+  df3=DataFrame(mynn, columns= ["Layer Name","FLOPs","Self CPU total","CPU Total","GPU Total","Input Features","Output Features","Dict Size of Emb","Emb Vector Size","Norm Size"])
   if want_op_file==True:
     export_csv = df3.to_csv (r'output_file.csv', index = None, header=True)
   else:
